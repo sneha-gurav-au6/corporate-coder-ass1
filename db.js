6da12177eabca2815d36c9a31f655e0dff54abfc
unknown <snehagurav14.sg@gmail.com>
2020-11-27 13:01:03 +0530

    push

--- db.js
+++ db.js
@@ -2,16 +2,18 @@
 
 const mongoose = require("mongoose");
 
-
 mongoose
-  .connect(process.env.MONGOOSE_URI , {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-  })
-  .then(function () {
-    console.log("Mongo db compass connected");
-  })
-  .catch(function (err) {
-    console.log(err.message);
-  })
+    .connect(
+        `mongodb+srv://snehagurav:sneha@zero.pip3u.mongodb.net/todo-assignment?retryWrites=true&w=majority`,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+        }
+    )
+    .then(function () {
+        console.log("Mongo db compass connected");
+    })
+    .catch(function (err) {
+        console.log(err.message);
+    });
